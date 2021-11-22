b9369e30b8360ae61f99f290e3f3e9e1808e5beb
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-11-21 23:25:30 -0300

    :sparkles: Added query selectors for input fields

---
+++ Fundamentals of Web Development/block-05-js-dom-events-and-web-storage/day-04-js-web-storage/script.js
@@ -0,0 +1,5 @@
+const bgColor = document.querySelector('#bg-color');
+const textColor = document.querySelector('#text-color');
+const fontSize = document.querySelector('#font-size');
+const lineHeight = document.querySelector('#line-height');
+const fontFamily = document.querySelector('#font-family');
