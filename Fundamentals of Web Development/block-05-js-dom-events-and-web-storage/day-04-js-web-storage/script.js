83e3e7fd25af03a18c4482072186cf0856a60724
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-11-22 01:37:37 -0300

    :bug: Fixing localstorage persistential

--- Fundamentals of Web Development/block-05-js-dom-events-and-web-storage/day-04-js-web-storage/script.js
+++ Fundamentals of Web Development/block-05-js-dom-events-and-web-storage/day-04-js-web-storage/script.js
@@ -10,21 +10,16 @@ const preferences = {
 
 function addInputListeners() {
     inputList.forEach((input) => {
-        input.addEventListener('keyup', (event) => {
-            if (event.key === 'Enter') {
-                const e = event.target;
-                preferences[e.id] = e.value;
-                addPreferencesToLocalStorage();
-            };
-        });
+        input.addEventListener('keyup', (event) => { if (event.key === 'Enter') addPreferencesToLocalStorage(event.target) });
     });
-};
-
-function addPreferencesToLocalStorage() {
-    const oldPreferences = JSON.parse(localStorage.getItem('preferences'))
-    localStorage.setItem('preferences', JSON.stringify(preferences));
-    addPreferencesInDOM();
 }
+
+function addPreferencesToLocalStorage(target) {
+    const oldPreferences = JSON.parse(localStorage.getItem('preferences'))
+    const newPreferences = { ...oldPreferences, [target.id]: target.value };
+    localStorage.setItem('preferences', JSON.stringify(newPreferences));
+    addPreferencesInDOM();
+};
 
 function initialRenderization() {
     if (!localStorage.getItem('preferences')) localStorage.setItem('preferences', JSON.stringify(preferences));
