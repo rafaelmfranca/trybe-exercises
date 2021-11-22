d8aa55fd06b4e03d36e474d5f1073f9c09473b5e
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-11-22 10:58:34 -0300

    :sparkles: Set preferences in DOM dinamically w/ localStorage

--- Fundamentals of Web Development/block-05-js-dom-events-and-web-storage/day-04-js-web-storage/script.js
+++ Fundamentals of Web Development/block-05-js-dom-events-and-web-storage/day-04-js-web-storage/script.js
@@ -1,8 +1,8 @@
 const inputList = document.querySelectorAll('input[type="text"]');
 
 const preferences = {
-    bgColor: '',
-    textColor: '',
+    backgroundColor: '',
+    color: '',
     fontSize: '',
     lineHeight: '',
     fontFamily: ''
@@ -12,17 +12,32 @@ function addInputListeners() {
     inputList.forEach((input) => {
         input.addEventListener('keyup', (event) => { if (event.key === 'Enter') addPreferencesToLocalStorage(event.target) });
     });
-}
+};
 
 function addPreferencesToLocalStorage(target) {
-    const oldPreferences = JSON.parse(localStorage.getItem('preferences'))
+    const oldPreferences = JSON.parse(localStorage.getItem('preferences'));
     const newPreferences = { ...oldPreferences, [target.id]: target.value };
     localStorage.setItem('preferences', JSON.stringify(newPreferences));
-    addPreferencesInDOM();
+    target.value = '';
+    addPreferencesInDOM(newPreferences);
+};
+
+function addPreferencesInDOM(preferences) {
+    const textContent = document.querySelector('#text-content');
+    const contentStyle = textContent.style;
+    if (preferences.backgroundColor) contentStyle.backgroundColor = preferences.backgroundColor;
+    if (preferences.color) contentStyle.color = preferences.color;
+    if (preferences.fontSize) contentStyle.fontSize = +preferences.fontSize + 'px';
+    if (preferences.lineHeight) contentStyle.lineHeight = +preferences.lineHeight + 'px';
+    if (preferences.fontFamily) contentStyle.fontFamily = preferences.fontFamily;
 };
 
 function initialRenderization() {
     if (!localStorage.getItem('preferences')) localStorage.setItem('preferences', JSON.stringify(preferences));
+    else {
+        const preferences = JSON.parse(localStorage.getItem('preferences'));
+        addPreferencesInDOM(preferences);
+    };
 };
 
 addInputListeners();
