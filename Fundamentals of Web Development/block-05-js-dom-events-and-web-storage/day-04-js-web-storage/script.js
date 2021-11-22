865289f13c1e5dc6733370472649f0494e77fb51
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-11-22 01:23:14 -0300

    :sparkles: Added listeners and set user preferences in localstorage

--- Fundamentals of Web Development/block-05-js-dom-events-and-web-storage/day-04-js-web-storage/script.js
+++ Fundamentals of Web Development/block-05-js-dom-events-and-web-storage/day-04-js-web-storage/script.js
@@ -1,5 +1,35 @@
-const bgColor = document.querySelector('#bg-color');
-const textColor = document.querySelector('#text-color');
-const fontSize = document.querySelector('#font-size');
-const lineHeight = document.querySelector('#line-height');
-const fontFamily = document.querySelector('#font-family');
+const inputList = document.querySelectorAll('input[type="text"]');
+
+const preferences = {
+    bgColor: '',
+    textColor: '',
+    fontSize: '',
+    lineHeight: '',
+    fontFamily: ''
+};
+
+function addInputListeners() {
+    inputList.forEach((input) => {
+        input.addEventListener('keyup', (event) => {
+            if (event.key === 'Enter') {
+                const e = event.target;
+                preferences[e.id] = e.value;
+                addPreferencesToLocalStorage();
+            };
+        });
+    });
+};
+
+function addPreferencesToLocalStorage() {
+    const oldPreferences = JSON.parse(localStorage.getItem('preferences'))
+    localStorage.setItem('preferences', JSON.stringify(preferences));
+    addPreferencesInDOM();
+}
+
+function initialRenderization() {
+    if (!localStorage.getItem('preferences')) localStorage.setItem('preferences', JSON.stringify(preferences));
+};
+
+addInputListeners();
+
+window.onload = () => { initialRenderization(); };
