65c738fe973a8b1e4ec2f02a3dd623a549d9589f
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-12-09 20:35:28 -0300

    :sparkles: Added drawResult function

--- Fundamentals of Web Development/block-08-es6-js-hofs/day-01-introduction-to-hofs/practice.js
+++ Fundamentals of Web Development/block-08-es6-js-hofs/day-01-introduction-to-hofs/practice.js
@@ -1,5 +1,4 @@
 // Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
-
 const newEmployees = (callback) => {
   const employees = {
     id1: callback('Pedro Guerra'),
@@ -9,9 +8,22 @@ const newEmployees = (callback) => {
   return employees;
 };
 
-const generateEmployee = (fullName) => {
-  return { name: fullName, email: `${fullName.toLowerCase().split(' ').join('_')}@trybe.com` };
-};
+const generateEmployee = (fullName) => ({ name: fullName, email: `${fullName.toLowerCase().split(' ').join('_')}@trybe.com` });
 
 console.log(generateEmployee('Rafael Giori'));
 console.log(newEmployees(generateEmployee));
+
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const drawResult = (betNumber, callback) => callback(betNumber, getRandomNumber(1, 5)) ? 'Parabéns você ganhou' : 'Tente novamente';
+const verifyResult = (number, randomNumber) => number === randomNumber;
+
+console.log(drawResult(5, verifyResult));
+
+// Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
