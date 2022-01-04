723fc8efbfa8f93cb63a65dd4219d3bedda751dc
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-01-04 14:47:37 -0300

    :sparkles: 1st exercise - flatten an array

---
+++ Fundamentals of Web Development/block-08-es6-js-hofs/day-04-js-es6-hofs-reduce/practice.js
@@ -0,0 +1,5 @@
+// Todos os exercícios devem ser realizados utilizando reduce , e se necessário outra HOF , a informação será citada no enunciado.
+// 1 - Dada uma matriz, transforme em um array.
+const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
+const flatten = () => arrays.reduce((acc, item) => acc.concat(item), []);
+console.log(flatten());
