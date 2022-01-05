e1d3b34241ebb17eef5b6ddc8efcf5012ccf09ae
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-01-05 15:23:32 -0300

    :bulb: Added exercise statement

--- Fundamentals of Web Development/block-08-es6-js-hofs/day-05-es6-js-spread-rest-destructuring-and-more/exercise2.js
+++ Fundamentals of Web Development/block-08-es6-js-hofs/day-05-es6-js-spread-rest-destructuring-and-more/exercise2.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
 const sum = (...args) => args.reduce((a, b) => a + b, 0);
 
 assert.strictEqual(sum(), 0);
