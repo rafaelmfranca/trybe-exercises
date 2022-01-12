1da3942a0a534272e515399a1afc39abe0692372
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-01-12 19:22:04 -0300

    :recycle: Refactoring adding exception blocks

--- Fundamentals of Web Development/block-09-es6-js-exception-flow-and-objects/day-02-js-assync-await-fetch-api/api.js
+++ Fundamentals of Web Development/block-09-es6-js-exception-flow-and-objects/day-02-js-assync-await-fetch-api/api.js
@@ -1,30 +1,43 @@
 const fetchCryptoApi = async () => {
-  const url = 'https://api.coincap.io/v2/assets';
-  const response = await fetch(url);
-  const { data } = await response.json();
-  return data.slice(0, 10);
+  try {
+    const url = 'https://api.coincap.io/v2/assets';
+    const response = await fetch(url);
+    const { data } = await response.json();
+    return data.slice(0, 10);
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 const fetchCurrencyApi = async () => {
-  const url =
-    'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json';
-  const response = await fetch(url);
-  const {
-    usd: { brl },
-  } = await response.json();
-  return brl;
+  try {
+    const url =
+      'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json';
+
+    const response = await fetch(url);
+    const {
+      usd: { brl },
+    } = await response.json();
+    return brl;
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 const renderData = async () => {
-  const firstTen = await fetchCryptoApi();
-  const brlValue = await fetchCurrencyApi();
-  const cryptoContainer = document.getElementById('cryptoContainer');
+  try {
+    const firstTen = await fetchCryptoApi();
+    const brlValue = await fetchCurrencyApi();
+    const cryptoContainer = document.getElementById('cryptoContainer');
 
-  firstTen.forEach(({ name, symbol, priceUsd }) => {
-    const usdValue = Number(priceUsd);
-    const li = `<li>${name} ${symbol}: R$ ${(usdValue * brlValue).toFixed(2)}</li>`;
-    cryptoContainer.insertAdjacentHTML('beforeend', li);
-  });
+    firstTen.forEach(({ name, symbol, priceUsd }) => {
+      const usdValue = Number(priceUsd);
+      const li = `<li>${name} ${symbol}: R$ ${(usdValue * brlValue).toFixed(2)}</li>`;
+      cryptoContainer.insertAdjacentHTML('beforeend', li);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 window.onload = () => renderData();
