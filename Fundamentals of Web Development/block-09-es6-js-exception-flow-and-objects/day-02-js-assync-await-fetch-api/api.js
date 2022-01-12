55959e092365471aae47dbea550be0e7ddb54363
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-01-12 16:28:27 -0300

    :recycle: Refactoring exercise 2

--- Fundamentals of Web Development/block-09-es6-js-exception-flow-and-objects/day-02-js-assync-await-fetch-api/api.js
+++ Fundamentals of Web Development/block-09-es6-js-exception-flow-and-objects/day-02-js-assync-await-fetch-api/api.js
@@ -1,17 +1,23 @@
 const API_URL = 'https://api.coincap.io/v2/assets';
-const cryptoContainer = document.getElementById('cryptoContainer');
 
-const fetchApi = () => {
-  fetch(API_URL)
+const fetchApi = async () => {
+  const data = await fetch(API_URL)
     .then((response) => response.json())
-    .then(({ data }) => {
-      const firstTen = data.slice(0, 10);
-      firstTen.forEach(({ name, symbol, priceUsd }) => {
-        const li = `<li>${name} ${symbol}: ${Number(priceUsd).toFixed(2)}</li>`;
-        cryptoContainer.insertAdjacentHTML('beforeend', li);
-      });
-    })
-    .catch((error) => console.error(error));
+    .then(({ data }) => data)
+    .catch((error) => error.toString());
+
+  return data;
 };
 
-window.onload = () => fetchApi();
+const renderData = async () => {
+  const data = await fetchApi();
+  const cryptoContainer = document.getElementById('cryptoContainer');
+  const firstTen = data.slice(0, 10);
+
+  firstTen.forEach(({ name, symbol, priceUsd }) => {
+    const li = `<li>${name} ${symbol}: ${Number(priceUsd).toFixed(2)}</li>`;
+    cryptoContainer.insertAdjacentHTML('beforeend', li);
+  });
+};
+
+window.onload = () => renderData();
