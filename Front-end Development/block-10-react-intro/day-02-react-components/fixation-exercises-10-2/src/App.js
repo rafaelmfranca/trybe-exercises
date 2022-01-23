30cf3e723daf0ee2328e33f437555749faee9779
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-01-23 17:27:49 -0300

    :sparkles: Create Image component

--- Front-end Development/block-10-react-intro/day-02-react-components/fixation-exercises-10-2/src/App.js
+++ Front-end Development/block-10-react-intro/day-02-react-components/fixation-exercises-10-2/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Image from './Image';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>hello, world!</h1>
+      <Image
+        source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
+        alternativeText="Cute cat staring"
+      />
     </div>
   );
 };
