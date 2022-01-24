0c88aba3a40b8765cf80dfebc4542ab65576b1fb
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-01-23 21:28:10 -0300

    :sparkles: Components composition fixation exercise

--- Front-end Development/block-10-react-intro/day-02-react-components/fixation-exercises-10-2/src/App.js
+++ Front-end Development/block-10-react-intro/day-02-react-components/fixation-exercises-10-2/src/App.js
@@ -1,29 +1,36 @@
+import React from 'react';
 import './App.css';
-import UserProfile from './UserProfile';
+import Order from './Order';
 
-const App = () => {
-  const joao = {
-    id: 102,
-    name: 'João',
-    email: 'joao@gmail.com',
-    avatar:
-      'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png',
-  };
+class App extends React.Component {
+  render() {
+    const products = [
+      {
+        id: 102,
+        user: 'cena@gmail.com',
+        product: 'Razer Headphone',
+        price: {
+          value: 99.99,
+          currency: 'dollars',
+        },
+      },
+      {
+        id: 77,
+        user: 'cena@gmail.com',
+        product: 'Monster 500mL',
+        price: {
+          value: 9.99,
+          currency: 'dollars',
+        },
+      },
+    ];
 
-  const amelia = {
-    id: 77,
-    name: 'Amélia',
-    email: 'amelia@gmail.com',
-    avatar:
-      'https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png',
-  };
-
-  return (
-    <div className="App">
-      <UserProfile user={joao} />
-      <UserProfile user={amelia} />
-    </div>
-  );
-};
+    return products.map((product) => (
+      <>
+        <Order key={product.id} order={product} />
+      </>
+    ));
+  }
+}
 
 export default App;
--- Front-end Development/block-10-react-intro/day-02-react-components/fixation-exercises-10-2/src/Image.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Image extends Component {
-  render() {
-    const { source, alternativeText } = this.props;
-
-    return <img src={source} alt={alternativeText} />;
-  }
-}
