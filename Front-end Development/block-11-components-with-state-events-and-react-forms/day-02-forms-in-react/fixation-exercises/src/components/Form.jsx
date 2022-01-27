2e779add1c57b0835279a5abf3872c1f65c7ff90
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-01-27 16:00:28 -0300

    :sparkles: Added fieldsets

--- Front-end Development/block-11-components-with-state-events-and-react-forms/day-02-forms-in-react/fixation-exercises/src/components/Form.jsx
+++ Front-end Development/block-11-components-with-state-events-and-react-forms/day-02-forms-in-react/fixation-exercises/src/components/Form.jsx
@@ -41,66 +41,72 @@ export default class Form extends Component {
           Estados e React - Tecnologia fantástica ou reagindo a regionalismos?
         </h1>
         <form className="Form" onSubmit={this.handleSubmit}>
-          <label htmlFor="name">
-            Nome:
-            <input
-              id="name"
-              name="name"
-              type="text"
-              onChange={this.handleChange}
-              value={name}
-            />
-          </label>
+          <fieldset>
+            <legend>Informações pessoais</legend>
+            <label htmlFor="name">
+              Nome:
+              <input
+                id="name"
+                name="name"
+                type="text"
+                onChange={this.handleChange}
+                value={name}
+              />
+            </label>
 
-          <label htmlFor="email">
-            Email:
-            <input
-              id="email"
-              name="email"
-              type="email"
-              onChange={this.handleChange}
-              value={email}
-            />
-          </label>
+            <label htmlFor="email">
+              Email:
+              <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={this.handleChange}
+                value={email}
+              />
+            </label>
 
-          <label htmlFor="age">
-            Idade:
-            <select
-              id="age"
-              name="age"
-              onChange={this.handleChange}
-              value={age}
-            >
-              <option value="">Selecione</option>
-              <option value="adult">Maior que 18</option>
-              <option value="underage">Menor que 18</option>
-            </select>
-          </label>
+            <label htmlFor="age">
+              Idade:
+              <select
+                id="age"
+                name="age"
+                onChange={this.handleChange}
+                value={age}
+              >
+                <option value="">Selecione</option>
+                <option value="adult">Maior que 18</option>
+                <option value="underage">Menor que 18</option>
+              </select>
+            </label>
+          </fieldset>
 
-          <label htmlFor="anecdote">
-            Anedota:
-            <textarea
-              id="anecdote"
-              name="anecdote"
-              onChange={this.handleChange}
-              value={anecdote}
-            />
-          </label>
+          <fieldset>
+            <legend>Texto e arquivos</legend>
+            <label htmlFor="anecdote">
+              Anedota:
+              <textarea
+                id="anecdote"
+                name="anecdote"
+                onChange={this.handleChange}
+                value={anecdote}
+              />
+            </label>
 
-          <label htmlFor="terms">
-            <input
-              id="terms"
-              type="checkbox"
-              name="terms"
-              onChange={this.handleChange}
-              checked={terms}
-            />
-            Concordo com termos e acordos
-          </label>
-          <label>
-            Upload file:
-            <input type="file" />
-          </label>
+            <label htmlFor="terms">
+              <input
+                id="terms"
+                type="checkbox"
+                name="terms"
+                onChange={this.handleChange}
+                checked={terms}
+              />
+              Concordo com termos e acordos
+            </label>
+            <label>
+              Upload file:
+              <input type="file" />
+            </label>
+          </fieldset>
           <button type="submit">Submit</button>
         </form>
       </div>
