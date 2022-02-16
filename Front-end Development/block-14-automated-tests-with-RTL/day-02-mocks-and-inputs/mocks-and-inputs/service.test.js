967e1053211800869724a1bc3c67f9bec7ea63c5
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-02-16 20:04:00 -0300

    :sparkles: 3rd exercise - part1

--- Front-end Development/block-14-automated-tests-with-RTL/day-02-mocks-and-inputs/mocks-and-inputs/service.test.js
+++ Front-end Development/block-14-automated-tests-with-RTL/day-02-mocks-and-inputs/mocks-and-inputs/service.test.js
@@ -1,7 +1,8 @@
-let { generateRandomNumber } = require('./service');
+const services = require('./service');
 
 describe('Testing generateRandomNumber func', () => {
   it('exercise 1 ', () => {
+    let { generateRandomNumber } = services;
     generateRandomNumber = jest.fn().mockReturnValue(10);
 
     generateRandomNumber();
@@ -11,6 +12,7 @@ describe('Testing generateRandomNumber func', () => {
   });
 
   it('exercise 2 ', () => {
+    let { generateRandomNumber } = services;
     // Original implementation
     expect(generateRandomNumber()).not.toBeNull();
 
@@ -21,5 +23,21 @@ describe('Testing generateRandomNumber func', () => {
     expect(generateRandomNumber).toHaveBeenCalled();
     expect(generateRandomNumber).toHaveBeenCalledTimes(1);
     expect(generateRandomNumber).toHaveBeenCalledWith(2, 1);
+  });
+
+  it('exercise 3 ', () => {
+    // Creating mock and replacing implementation to be a division
+    const mockMultiplication = jest
+      .spyOn(services, 'generateRandomNumber')
+      .mockImplementation((a, b, c) => a * b * c);
+
+    services.generateRandomNumber(3, 3, 3);
+    expect(mockMultiplication).toHaveBeenCalledTimes(1);
+    expect(mockMultiplication(3, 3, 3)).toBe(27);
+    expect(mockMultiplication).toHaveBeenCalledTimes(2);
+    expect(mockMultiplication).toHaveBeenCalledWith(3, 3, 3);
+
+    services.generateRandomNumber.mockRestore();
+    expect(services.generateRandomNumber()).not.toBeNull();
   });
 });
