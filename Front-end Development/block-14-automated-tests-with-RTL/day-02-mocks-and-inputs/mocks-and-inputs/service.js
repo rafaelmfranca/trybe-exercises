607048625cbfe2d70bb5c856fcb533ea075c9ef2
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-02-16 19:29:13 -0300

    :sparkles: 2nd exercise - part1

--- Front-end Development/block-14-automated-tests-with-RTL/day-02-mocks-and-inputs/mocks-and-inputs/service.js
+++ Front-end Development/block-14-automated-tests-with-RTL/day-02-mocks-and-inputs/mocks-and-inputs/service.js
@@ -2,4 +2,8 @@ function generateRandomNumber() {
   return Math.floor(Math.random() * 100);
 }
 
+// function division(a, b) {
+//   return a / b;
+// }
+
 module.exports = { generateRandomNumber };
--- Front-end Development/block-14-automated-tests-with-RTL/day-02-mocks-and-inputs/mocks-and-inputs/service.test.js
+++ Front-end Development/block-14-automated-tests-with-RTL/day-02-mocks-and-inputs/mocks-and-inputs/service.test.js
@@ -9,4 +9,17 @@ describe('Testing generateRandomNumber func', () => {
     expect(generateRandomNumber()).toBe(10);
     expect(generateRandomNumber).toHaveBeenCalledTimes(2);
   });
+
+  it('exercise 2 ', () => {
+    // Original implementation
+    expect(generateRandomNumber()).not.toBeNull();
+
+    // Creating mock and replacing implementation to be a division
+    generateRandomNumber = jest.fn().mockImplementation((a, b) => a / b);
+
+    expect(generateRandomNumber(2, 1)).toBe(2);
+    expect(generateRandomNumber).toHaveBeenCalled();
+    expect(generateRandomNumber).toHaveBeenCalledTimes(1);
+    expect(generateRandomNumber).toHaveBeenCalledWith(2, 1);
+  });
 });
