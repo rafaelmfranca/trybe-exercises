5588cf1c545bc93d6ee06999cf514be4a7508414
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-02-22 15:01:27 -0300

    :sparkles: Understanding actions, stores and reducers

---
+++ Front-end Development/block-15-state-management-with-redux/day-01-intro-to-redux/fixation-exercises/index.js
@@ -0,0 +1,30 @@
+const Redux = require('redux');
+
+const signIn = (email, password) => ({
+  type: 'LOGIN',
+  email,
+});
+
+const DEFAULT_STATE = {
+  login: false,
+  email: '',
+};
+
+const reducer = (state = DEFAULT_STATE, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        login: !state.login,
+        email: action.email,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+store.dispatch(signIn('test@test.com', 'test123'));
+
+console.log(store.getState());
