d9e67d41781f96df1a5e7aff0f5529bfe0064b09
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-02-26 12:00:32 -0300

    :sparkles: Customers list page implemented

--- Front-end Development/block-15-state-management-with-redux/day-03-using-redux-in-react-practice/customer-base/src/pages/Customers.jsx
+++ Front-end Development/block-15-state-management-with-redux/day-03-using-redux-in-react-practice/customer-base/src/pages/Customers.jsx
@@ -1,16 +1,37 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const ReisterLink = () => <Link to="/register">Register page</Link>;
 class Customers extends Component {
   render() {
-    const { loggedUser } = this.props;
+    const { loggedUser, customersList } = this.props;
+    const userIsLoggedIn = Object.keys(loggedUser).length > 0;
+    const listIsFilled = customersList.length > 0;
     return (
       <div>
-        {!Object.keys(loggedUser).length ? (
-          <h1>Login não efetuado</h1>
-        ) : (
-          <h1>Customers</h1>
+        {userIsLoggedIn && listIsFilled && (
+          <>
+            <ul>
+              {customersList.map(({ name, age, email }) => (
+                <li>
+                  <p>{name}</p>
+                  <p>{age}</p>
+                  <p>{email}</p>
+                </li>
+              ))}
+            </ul>
+            <ReisterLink />
+          </>
         )}
+        {userIsLoggedIn && !listIsFilled && (
+          <>
+            <h1>Nenhum cliente cadastrado</h1>
+            <ReisterLink />
+          </>
+        )}
+        {!userIsLoggedIn && <h1>Login não efetuado</h1>}
       </div>
     );
   }
@@ -22,6 +43,7 @@ Customers.propTypes = {
 
 const mapStateToProps = (state) => ({
   loggedUser: state.loginReducer,
+  customersList: state.registerReducer,
 });
 
 export default connect(mapStateToProps)(Customers);
