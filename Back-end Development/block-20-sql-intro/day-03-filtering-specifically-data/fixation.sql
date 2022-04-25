0ed5869c01600b7fd98d8a7b5fd9a6db6dbc91a8
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-04-25 17:04:54 -0300

    :sparkles: Update fixation exercises/20.3

--- Back-end Development/block-20-sql-intro/day-03-filtering-specifically-data/fixation.sql
+++ Back-end Development/block-20-sql-intro/day-03-filtering-specifically-data/fixation.sql
@@ -125,3 +125,70 @@ FROM
 WHERE
   description LIKE '%monkey%'
   AND description LIKE '%sumo%';
+
+--1
+SELECT
+  first_name,
+  last_name,
+  email
+FROM
+  sakila.customer
+WHERE
+  last_name IN (
+    'hicks',
+    'crawford',
+    'henry',
+    'boyd',
+    'mason',
+    'morales',
+    'kennedy'
+  )
+ORDER BY
+  first_name;
+
+--2
+SELECT
+  email
+FROM
+  sakila.customer
+WHERE
+  address_id BETWEEN 172
+  AND 176
+ORDER BY
+  email;
+
+--3
+SELECT
+  COUNT(*) AS payment_count
+FROM
+  sakila.payment
+WHERE
+  DATE(payment_date) BETWEEN '2005/05/01'
+  AND '2005/08/01';
+
+--4
+SELECT
+  title,
+  release_year,
+  rental_duration
+FROM
+  sakila.film
+WHERE
+  rental_duration BETWEEN 3
+  AND 6
+ORDER BY
+  rental_duration DESC,
+  title;
+
+--5
+SELECT
+  title,
+  rating
+FROM
+  sakila.film
+WHERE
+  rating IN ('G', 'PG', 'PG-13')
+ORDER BY
+  title
+LIMIT
+  500;
