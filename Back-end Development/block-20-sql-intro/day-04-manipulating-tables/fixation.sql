c47263d056d7a15d956de6826e553d112058d3d3
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-04-26 18:30:32 -0300

    :sparkles: Update fixation exercises/20.4

--- Back-end Development/block-20-sql-intro/day-04-manipulating-tables/fixation.sql
+++ Back-end Development/block-20-sql-intro/day-04-manipulating-tables/fixation.sql
@@ -49,3 +49,41 @@ INSERT INTO
   sakila.store (manager_staff_id, address_id)
 VALUES
   (3, 3);
+
+--1
+UPDATE
+  sakila.actor
+SET
+  first_name = 'JULES'
+WHERE
+  first_name = 'JULIA';
+
+--2
+UPDATE
+  sakila.category
+SET
+  name = 'Science Fiction'
+WHERE
+  name = 'Sci-Fi';
+
+--3
+UPDATE
+  sakila.film
+SET
+  rental_rate = 25
+WHERE
+  length > 100
+  AND rating IN ('G', 'PG', 'PG-13')
+  AND replacement_cost > 20;
+
+--4
+UPDATE
+  sakila.film
+SET
+  rental_rate = (
+    CASE
+      WHEN length BETWEEN 1
+      AND 100 THEN 10
+      WHEN length > 100 THEN 20
+    END
+  );
