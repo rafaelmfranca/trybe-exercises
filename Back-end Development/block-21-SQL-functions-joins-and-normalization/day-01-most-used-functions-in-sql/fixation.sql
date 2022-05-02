7378be1ff62e569c5d1efa8ca85f6d23d27a73a9
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-05-02 17:11:01 -0300

    :sparkles: Update fixation exercises/21.1

--- Back-end Development/block-21-SQL-functions-joins-and-normalization/day-01-most-used-functions-in-sql/fixation.sql
+++ Back-end Development/block-21-SQL-functions-joins-and-normalization/day-01-most-used-functions-in-sql/fixation.sql
@@ -51,3 +51,106 @@ SELECT
   END AS 'público-alvo'
 FROM
   sakila.film;
+
+--1
+SELECT
+  ROUND(15 + (RAND() * 5));
+
+--2
+SELECT
+  ROUND(15.7515971, 5);
+
+--3
+SELECT
+  FLOOR(39.494);
+
+--4
+SELECT
+  CEIL(85.234);
+
+--1
+SELECT
+  DATEDIFF('2030-01-20', NOW());
+
+--2
+SELECT
+  TIMEDIFF('10:25:45', '11:00:00');
+
+--1
+SELECT
+  AVG(length) AS 'Média de Duração',
+  MIN(length) AS 'Duração Mínima',
+  MAX(length) AS 'Duração Máxima',
+  SUM(length) AS 'Tempo de Exibição Total',
+  COUNT(*) AS 'Filmes Registrados'
+FROM
+  sakila.film;
+
+--1
+SELECT
+  active,
+  COUNT(*)
+FROM
+  sakila.customer
+GROUP BY
+  active;
+
+--2
+SELECT
+  store_id,
+  active,
+  COUNT(*)
+FROM
+  sakila.customer
+GROUP BY
+  store_id,
+  active
+ORDER BY
+  store_id;
+
+--3
+SELECT
+  rating,
+  AVG(rental_rate) AS avg_rental_dur
+FROM
+  sakila.film
+GROUP BY
+  rating
+ORDER BY
+  avg_rental_dur DESC;
+
+--4
+SELECT
+  district,
+  COUNT(*) AS address_count
+FROM
+  sakila.address
+GROUP BY
+  district
+ORDER BY
+  address_count DESC;
+
+--1
+SELECT
+  rating,
+  AVG(length) AS avg_rating
+FROM
+  sakila.film
+GROUP BY
+  rating
+HAVING
+  avg_rating BETWEEN 115.0
+  AND 121.50;
+
+--2
+SELECT
+  rating,
+  SUM(replacement_cost) total_replacement_cost
+FROM
+  sakila.film
+GROUP BY
+  rating
+HAVING
+  total_replacement_cost > 3950.50
+ORDER BY
+  total_replacement_cost;
