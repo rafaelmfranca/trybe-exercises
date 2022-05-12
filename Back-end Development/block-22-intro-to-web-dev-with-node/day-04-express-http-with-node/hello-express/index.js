a12eed81b6fa8090e0b0f43e389af7dbe66aa47b
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-05-12 15:31:07 -0300

    :sparkles: Update fixation exercises/22.4

--- Back-end Development/block-22-intro-to-web-dev-with-node/day-04-express-http-with-node/hello-express/index.js
+++ Back-end Development/block-22-intro-to-web-dev-with-node/day-04-express-http-with-node/hello-express/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { recipes, drinks } = require('./data');
-const sortInAlphabeticalOrder = require('./utils');
+const { findItemById, sortInAlphabeticalOrder } = require('./utils');
 
 app.use(cors());
 
@@ -12,6 +12,24 @@ app.get('/drinks', (_, res) => {
 
 app.get('/recipes', (_, res) => {
   res.json(sortInAlphabeticalOrder(recipes));
+});
+
+app.get('/drinks/:id', (req, res) => {
+  const { id } = req.params;
+  const drink = findItemById(drinks, id);
+
+  return drink
+    ? res.json(drink)
+    : res.status(404).json({ error: 'Drink not found' });
+});
+
+app.get('/recipes/:id', (req, res) => {
+  const { id } = req.params;
+  const recipe = findItemById(recipes, id);
+
+  return recipe
+    ? res.json(recipe)
+    : res.status(404).json({ error: 'Recipe not found' });
 });
 
 app.listen(3001, () => {
--- Back-end Development/block-22-intro-to-web-dev-with-node/day-04-express-http-with-node/hello-express/utils.js
+++ Back-end Development/block-22-intro-to-web-dev-with-node/day-04-express-http-with-node/hello-express/utils.js
@@ -2,4 +2,8 @@ function sortInAlphabeticalOrder(array) {
   return array.sort((a, b) => a.name.localeCompare(b.name));
 }
 
-module.exports = sortInAlphabeticalOrder;
+function findItemById(array, id) {
+  return array.find((item) => item.id === Number(id));
+}
+
+module.exports = { sortInAlphabeticalOrder, findItemById };
