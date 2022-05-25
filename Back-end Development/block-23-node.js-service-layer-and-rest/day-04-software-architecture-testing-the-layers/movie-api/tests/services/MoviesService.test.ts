9341c00f298b25fbb622c9489abc6d8a46d035e8
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-05-24 21:27:41 -0300

    ✅ (test): Add test for getById method on movies service

--- Back-end Development/block-23-node.js-service-layer-and-rest/day-04-software-architecture-testing-the-layers/movie-api/tests/services/MoviesService.test.ts
+++ Back-end Development/block-23-node.js-service-layer-and-rest/day-04-software-architecture-testing-the-layers/movie-api/tests/services/MoviesService.test.ts
@@ -50,3 +50,66 @@ describe('Insert a new movie into BD', () => {
     });
   });
 });
+
+describe('Get movie from DB by ID', () => {
+  const movieId = 1;
+  describe('when movie exists on DB', () => {
+    before(() => {
+      sinon.stub(MoviesModel, 'getById').resolves({
+        title: 'Movie title',
+        directed_by: 'John Doe',
+        release_year: 1997,
+      });
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('returns an object', async () => {
+      const response = await MoviesService.getById(movieId);
+      expect(response).to.be.an('object');
+    });
+
+    it('the object has the correct properties', async () => {
+      const response = await MoviesService.getById(movieId);
+      expect(response).to.have.a.property('title');
+      expect(response).to.have.a.property('directed_by');
+      expect(response).to.have.a.property('release_year');
+    });
+
+    it('the object has the correct values', async () => {
+      const response = await MoviesService.getById(movieId);
+      expect(response.title).to.equal('Movie title');
+      expect(response.directed_by).to.equal('John Doe');
+      expect(response.release_year).to.equal(1997);
+    });
+  });
+
+  describe('when movie not exists on DB', () => {
+    const movieId = 1;
+
+    before(() => {
+      sinon.stub(MoviesModel, 'getById').resolves(null);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('returns an object', async () => {
+      const response = await MoviesService.getById(movieId);
+      expect(response).to.be.an('object');
+    });
+
+    it('the object has the correct propertie', async () => {
+      const response = await MoviesService.getById(movieId);
+      expect(response).to.have.a.property('message');
+    });
+
+    it('the object has the correct value', async () => {
+      const response = await MoviesService.getById(movieId);
+      expect(response.message).to.equal('Movie not found');
+    });
+  });
+});
