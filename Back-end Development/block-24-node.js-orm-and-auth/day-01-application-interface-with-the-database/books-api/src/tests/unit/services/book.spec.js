9097fa84173fc96b1e9523dfb0c1584f864a7d46
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-06-07 13:36:20 -0300

    ✅ (service): add more tests (getById and create)

--- Back-end Development/block-24-node.js-orm-and-auth/day-01-application-interface-with-the-database/books-api/src/tests/unit/services/book.spec.js
+++ Back-end Development/block-24-node.js-orm-and-auth/day-01-application-interface-with-the-database/books-api/src/tests/unit/services/book.spec.js
@@ -5,8 +5,8 @@ const { Book } = require('../../../models');
 const bookMock = require('../../__mocks__/bookMock');
 const bookService = require('../../../services/books.service');
 
-describe('Books service', () => {
-  describe('getAll() func', () => {
+describe('✅ Books service', () => {
+  describe('➡️ getAll() func', () => {
     const findAll = sinon.stub(Book, 'findAll');
     let books;
 
@@ -46,6 +46,71 @@ describe('Books service', () => {
       it('should return an filled array', () => {
         expect(books).to.be.an('array').to.be.deep.equal([bookMock]);
       });
+    });
+  });
+
+  describe('➡️ getById() func', () => {
+    const findByPk = sinon.stub(Book, 'findByPk');
+    let books;
+
+    describe('when there is no book registered', async () => {
+      before(async () => {
+        findByPk.resolves(null);
+        books = await bookService.getById(1);
+      });
+
+      after(() => {
+        findByPk.reset();
+      });
+
+      it('should call Book.findByPk()', () => {
+        expect(findByPk.calledOnce).to.be.true;
+      });
+
+      it('should return null', () => {
+        expect(books).to.be.null;
+      });
+    });
+
+    describe('when there is a book with specified id', async () => {
+      before(async () => {
+        findByPk.resolves(bookMock);
+        books = await bookService.getById();
+      });
+
+      after(() => {
+        findByPk.restore();
+      });
+
+      it('should call Book.findByPk()', () => {
+        expect(findByPk.calledOnce).to.be.true;
+      });
+
+      it('should return an object with specified book', () => {
+        expect(books).to.be.an('object').to.be.deep.equal(bookMock);
+      });
+    });
+  });
+
+  describe('➡️ create() func', () => {
+    const create = sinon.stub(Book, 'create');
+    let books;
+
+    before(async () => {
+      create.resolves({ id: 2, ...bookMock });
+      books = await bookService.create(bookMock);
+    });
+
+    after(() => {
+      create.reset();
+    });
+
+    it('should call Book.create()', () => {
+      expect(create.calledOnce).to.be.true;
+    });
+
+    it('should return an object with the created book', () => {
+      expect(books).to.includes(bookMock);
     });
   });
 });
