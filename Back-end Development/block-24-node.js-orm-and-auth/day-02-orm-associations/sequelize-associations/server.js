bf06e6682a5593958ddac17755530bb5e9729d04
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-06-07 19:10:16 -0300

    🎉 (sequelize-associations): initial commit

---
+++ Back-end Development/block-24-node.js-orm-and-auth/day-02-orm-associations/sequelize-associations/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const errorMiddleware = require('./src/middlewares/error');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(errorMiddleware);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
