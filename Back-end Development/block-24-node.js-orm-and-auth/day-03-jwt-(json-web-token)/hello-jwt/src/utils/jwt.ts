e5e726dae2e15657f2af83803f30f60549767316
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-06-08 23:34:07 -0300

    ✨ (utils): add verifyToken func

--- Back-end Development/block-24-node.js-orm-and-auth/day-03-jwt-(json-web-token)/hello-jwt/src/utils/jwt.ts
+++ Back-end Development/block-24-node.js-orm-and-auth/day-03-jwt-(json-web-token)/hello-jwt/src/utils/jwt.ts
@@ -1,9 +1,11 @@
 import { ISignInPayload } from '@types';
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 
 const secret = String(process.env.SECRET_KEY);
 
 export default {
   generateToken: (payload: ISignInPayload) =>
     sign(payload, secret, { expiresIn: '1h', algorithm: 'HS256' }),
+
+  verifyToken: (token: string) => verify(token, secret),
 };
